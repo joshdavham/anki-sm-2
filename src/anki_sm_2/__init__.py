6dd859714dfe63de825522217b7399b784d149eb
Joshua Hamilton <hamiltonjoshuadavid@gmail.com>
2024-10-24 14:29:43 -0500

    add basic module skeleton

---
+++ src/anki_sm_2/__init__.py
@@ -0,0 +1,1 @@
+from .anki_sm_2 import AnkiSM2Scheduler, Card, Rating, ReviewLog
---
+++ src/anki_sm_2/anki_sm_2.py
@@ -0,0 +1,20 @@
+from enum import IntEnum
+
+class Rating(IntEnum):
+
+    Again = 1
+    Hard = 2
+    Good = 3
+    Easy = 4
+
+# TODO: implement Card class
+class Card:
+    pass
+
+# TODO: implement ReviewLog class
+class ReviewLog:
+    pass
+
+# TODO: implement scheduler class
+class AnkiSM2Scheduler:
+    pass
