4e7d196896c538be312dad20cd90c0533099a33b
Joshua Hamilton <hamiltonjoshuadavid@gmail.com>
2024-10-28 16:03:40 -0500

    add State class

--- src/anki_sm_2/anki_sm_2.py
+++ src/anki_sm_2/anki_sm_2.py
@@ -1,11 +1,17 @@
 from enum import IntEnum
 
+class State(IntEnum):
+
+    Learning = 1
+    Review = 2
+    Relearing = 3
+
 class Rating(IntEnum):
 
-    Again = 1
-    Hard = 2
-    Good = 3
-    Easy = 4
+    Again = 1 # incorrect
+    Hard = 2 # correct - had doubts about answer/or took long time to recall
+    Good = 3 # correct - took some amount of mental effort to recall
+    Easy = 4 # correct - recalled effortlessly
 
 # TODO: implement Card class
 class Card:
